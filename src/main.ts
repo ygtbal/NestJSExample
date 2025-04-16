c6ce5a00e59443466040946c5c2a26f812fcc3d2
ygtbal <yigitbalalan@gmail.com>
2025-04-16 17:08:48 +0300

    swagger implementation is completed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configService } from './config/config.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (!configService.isProduction()) {
+    console.log('Swagger is enabled');
+    const document = SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Item API')
+        .setDescription('My Item API')
+        .build(),
+    );
+
+    SwaggerModule.setup('docs', app, document);
+  }
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
