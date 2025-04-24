0c05d300ed86f00f8e186d609e665b120497f3cd
ygtbal <yigitbalalan@gmail.com>
2025-04-24 16:19:42 +0300

    acc_item payment updated

--- src/acc_item/acc_item.service.ts
+++ src/acc_item/acc_item.service.ts
@@ -5,6 +5,7 @@ import { Acc } from '../model/acc.entity';
 import { Repository, DataSource } from 'typeorm';
 import { AccItemDto } from './acc_item.dto';
 import { Company } from '../model/company.entity';
+import { error } from 'console';
 
 @Injectable()
 export class AccItemService {
@@ -50,6 +51,53 @@ export class AccItemService {
         await manager.save(Company, company);
         return savedAccItem;
       }
+    });
+  }
+  public async update(id: string, dto: Partial<AccItem>): Promise<AccItem> {
+    return await this.dataSource.transaction(async (manager) => {
+      const accItem = await manager.findOne(AccItem, {
+        where: { id },
+        relations: ['acc', 'acc.company'],
+      });
+      if (!accItem) {
+        throw new Error(`Acc Item with id ${id} not found`);
+      }
+
+      const updatedAccItem = await manager.preload(AccItem, {
+        id,
+        ...dto,
+      });
+
+      if (!updatedAccItem) {
+        throw new Error(`Failed to preload AccItem with id ${id}`);
+      }
+      const differ = accItem.payment - (dto.payment ?? 0);
+      const total_payment = accItem.acc.total_payment - differ;
+      const accObj = { total_payment };
+      const updatedAcc = await manager.preload(Acc, {
+        id: accItem.acc.id,
+        ...accObj,
+      });
+      if (!updatedAcc) {
+        throw new Error(`Failed to preload Acc with id ${accItem.acc.id}`);
+      }
+      await manager.save(Acc, updatedAcc);
+      const company = accItem.acc.company;
+      const companyPaid = company.paid - differ;
+      const companyRes = company.totalAcc - companyPaid;
+      const compObj = {
+        paid: companyPaid,
+        rest: companyRes,
+      };
+      const updatedCompany = await manager.preload(Company, {
+        id: company.id,
+        ...compObj,
+      });
+      if (!updatedCompany) {
+        throw new error(`Company not found`);
+      }
+      await manager.save(Company, updatedCompany);
+      return await manager.save(AccItem, updatedAccItem);
     });
   }
 }
