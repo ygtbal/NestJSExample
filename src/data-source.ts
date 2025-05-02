433be7b24852c04656079d10c016078d33b2e785
ygtbal <yigitbalalan@gmail.com>
2025-05-02 18:37:19 +0300

    main.ts is reverted to old version

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,6 @@
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { configService } from './config/config.service';
 
 export const AppDataSource = new DataSource(
-  configService.getDataSourceOptions(),
+  configService.getTypeOrmConfig() as DataSourceOptions,
 );
